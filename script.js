eecb8bc60545e4d2a70841e98d8ef9f78344fc91
hildelio <hildelio@gmail.com>
2022-07-08 11:03:31 -0300

    cumprindo itens obrigatórios

--- script.js
+++ script.js
@@ -1,60 +1,56 @@
-const main = document.querySelector('#main');
-const colorPalette = document.createElement('div');
-colorPalette.id = 'color-palette';
+function startColors() {
+	const black = document.getElementById('preto');
+	const green = document.getElementById('verde');
+	const yellow = document.getElementById('amarelo');
+	const red = document.getElementById('vermelho');
+	black.style.backgroundColor = 'black';
+	green.style.backgroundColor = 'green';
+	yellow.style.backgroundColor = 'yellow';
+	red.style.backgroundColor = 'red';
+}
+startColors()
 
-function iniciaCores() {
-	const preto = document.getElementById('preto');
-	const verde = document.getElementById('verde');
-	const amarelo = document.getElementById('amarelo');
-	const vermelho = document.getElementById('vermelho');
-	preto.style.backgroundColor = 'black';
-	verde.style.backgroundColor = 'green';
-	amarelo.style.backgroundColor = 'yellow';
-	vermelho.style.backgroundColor = 'red';
+function picColor(color) {
+	const choosedColor = document.querySelector('.selected');
+	choosedColor.classList.remove('selected');
+	color.target.classList.add('selected');
 }
 
-function desenharQuadro(tamanho) {
-	const quadro =document.getElementById('pixel-board');
-	const dimensoes = tamanho * 50;
-	const xy = tamanho * tamanho;
-	quadro.style.width = `${dimensoes}px`;
-	quadro.style.maxWidth = `${dimensoes}px`;
+const activedColor = document.getElementById('color-palette');
+activedColor.addEventListener('click', picColor);
+
+function nowColor(clickEvent) {
+	const pickedColor = document.querySelector('.selected').style.backgroundColor;
+	const drawing = clickEvent.target;
+	drawing.style.backgroundColor = pickedColor;
+}
+
+function drawBoard(size) {
+	const board =document.getElementById('pixel-board');
+	const dimensoes = size * 50;
+	const xy = size * size;
+	board.style.width = `${dimensoes}px`;
+	board.style.maxWidth = `${dimensoes}px`;
 	for (let i = 0; i < (xy); i += 1) {
 		const pixel = document.createElement('div');
 		pixel.className = 'pixel';
-		quadro.appendChild(pixel);
+		board.appendChild(pixel);
 	}
 }
+drawBoard(5);
 
-desenharQuadro(5);
+const paintedBoard = document.getElementById('pixel-board');
+paintedBoard.addEventListener('click', nowColor);
 
-function escolheCor(cor) {
-	const corEscolhida = document.querySelector('.selected');
-	corEscolhida.classList.remove('selected');
-	cor.target.classList.add('selected');
-}
-
-const corAtiva = document.getElementById('color-palette');
-corAtiva.addEventListener('click', escolheCor);
-
-function corAtual(clickEvent) {
-	const corEscolhida = document.querySelector('.selected').style.backgroundColor;
-	const pintaQuadradinho = clickEvent.target;
-	pintaQuadradinho.style.backgroundColor = corEscolhida;
-}
-
-const quadradinhoPintado = document.getElementById('pixel-board');
-quadradinhoPintado.addEventListener('click', corAtual);
-
-function limpaTudo() {
-	const botaoLimpar = document.getElementById('clear-board');
-	botaoLimpar.addEventListener('click', () => {
-		const quadradinhos = document.querySelectorAll('.pixel');
-		for(let i = 0; i < quadradinhos.length; i += 1) {
-			quadradinhos[i].style.backgroundColor = 'white';
+function clearAll() {
+	const clearBotton = document.getElementById('clear-board');
+	clearBotton.addEventListener('click', () => {
+		const smallBoards = document.querySelectorAll('.pixel');
+		for(let i = 0; i < smallBoards.length; i += 1) {
+			smallBoards[i].style.backgroundColor = 'white';
 		}
 	})
 }
-limpaTudo();
+clearAll();
 
 const body = document.getElementById('body')
