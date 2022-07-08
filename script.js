246c41cd00aec65fbdd369efc30b8090028a02a0
hildelio <hildelio@gmail.com>
2022-07-07 23:30:32 -0300

    salvando avanços, ainda não consigo pintar os quadrinhos

--- script.js
+++ script.js
@@ -1,31 +1,16 @@
-// const header = document.querySelector('#header')
-// const h1 = document.createElement('h1')
-// h1.innerText = 'Paleta de cores';
-// h1.id = 'title'
-// header.appendChild(h1)
-
 const main = document.querySelector('#main');
-const colorPalette = document.createElement('div')
-colorPalette.id = 'color-palette'
-
-function corAleatoria() {
-	const chars = '0123456789ABCDEF'
-	let color = '#'
-	for (let i = 0; i < 6; i += 1) {
-		color += chars.charAt(Math.floor(Math.random() * chars.length))
-	}
-	return color;
-}
+const colorPalette = document.createElement('div');
+colorPalette.id = 'color-palette';
 
 function iniciaCores() {
 	const preto = document.getElementById('preto');
 	const verde = document.getElementById('verde');
 	const amarelo = document.getElementById('amarelo');
 	const vermelho = document.getElementById('vermelho');
-	preto.style.backgroundColor = corAleatoria();
-	verde.style.backgroundColor = corAleatoria();
-	amarelo.style.backgroundColor = corAleatoria();
-	vermelho.style.backgroundColor = corAleatoria();
+	preto.style.backgroundColor = 'black';
+	verde.style.backgroundColor = 'green';
+	amarelo.style.backgroundColor = 'yellow';
+	vermelho.style.backgroundColor = 'red';
 }
 
 function desenharQuadro(tamanho) {
@@ -55,7 +40,7 @@ corAtiva.addEventListener('click', escolheCor);
 function corAtual(clickEvent) {
 	const corEscolhida = document.querySelector('.selected').style.backgroundColor;
 	const pintaQuadradinho = clickEvent.target;
-	pintaQuadradinho.style.backgroundColor = corEscolhida
+	pintaQuadradinho.style.backgroundColor = corEscolhida;
 }
 
 const quadradinhoPintado = document.getElementById('pixel-board');
@@ -72,14 +57,4 @@ function limpaTudo() {
 }
 limpaTudo();
 
-// }
-// document.getElementById('btn-action').addEventListener('click', () => {
-// 	for (let i = 0; i < 5; i += 1) {
-// 		let color = generateColor()
-// 		let section_color = document.getElementById(`color-${i + 1}`)
-// 		section_color.style.backgroundColor = color
-// 		let text_color = document.getElementById(`text-color-${i + 1}`)
-// 		text_color.textContent = `${color}`
-// 	}
-// })
-
+const body = document.getElementById('body')
